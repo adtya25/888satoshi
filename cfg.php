2d83c70242c13bce4b41471a212917abed7542d3
adtya25 <79787334+adtya25@users.noreply.github.com>
2021-04-27 14:21:57 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,15 @@
+<?php
+
+// Perhatikan Tanda (')&(;) Jgn Terhapus Nanti Eror.!!
+//Masukan Data Data Yg Di Minta
+//slot ke satu  Data(UserAgen)
+//Slot Ke dua Data(Cookie)
+//Script Pakai Masa Aktif Untuk On kan Kembali Silahkan Shortlink
+//Seperti Di Awal Agar Balance Nya Masuk
+//Warning Di Larang Deycript Script Ini 😤😤😡😡😠
+
+
+//masukan user-agent
+$user = 'xxxxxxxxx';
+//masukan cookie
+$cookie = 'xxxxxxxx';
